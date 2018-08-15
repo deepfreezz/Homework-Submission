b19d109ca85601dbfa3dea3ac11e4d6fb6233d2e
deepfreezz <isaachailemariam@gmail.com>
2018-08-15 17:05:12 -0400

    week 5 homework

---
+++ Week 5/week5.js
@@ -0,0 +1,33 @@
+// Question 1
+console.log("Hellow  world")
+console.log("selam Alem")
+console.log("Hola Mundo")
+// Question 2
+console.log('I\'m awesome');
+// Question 3
+A.var x;
+B.console.log("undefined")
+C.console.log(x)
+D.var x=1;
+E.console.log("the value is 1")
+F.console.log(x)
+// Question 4
+var y="isaac"
+A. console.log("the value of y is isaac")
+B. console.log (y)
+C.var y="computer"
+D.console.log("the value of y is computer")
+E.consle.log(y)
+// Quesiont 5
+A.var z=7.25;
+B.console.log(z)
+C.var a=Math.round(z)
+D.console.log(a)
+E.if (a>z){
+  var x=a;
+
+}else{
+  var x=z;
+}
+F.console.log(x)
+Question
